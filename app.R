0611a4490ff327213a80c38e5c00d64d5b3e03b7
gwittebolle <52353111+gwittebolle@users.noreply.github.com>
2020-07-04 16:49:57 +0200

    Update app.R

--- app.R
+++ app.R
@@ -24,6 +24,14 @@ ui <- secure_app(fluidPage(
 server <- function(input, output) {
 
 
+  res_auth <- secure_server(
+    check_credentials = check_credentials(credentials)
+  )
+  
+  output$auth_output <- renderPrint({
+    reactiveValuesToList(res_auth)
+  })
+  
     
     output$ex1 <- DT::renderDataTable(
 
